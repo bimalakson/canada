59a3b91e48bfe4836eb0f2ed82224d24698e0e24
bimalakson <bimalakson@gmail.com>
2022-02-08 05:07:23 +0700

    Four

--- transaksi/urls.py
+++ transaksi/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('transaksi/', views.transaksi, name="transaksi"),
     path('keranjang/', views.keranjang, name="keranjang"),
     path('pembayaran/', views.pembayaran, name="pembayaran"),
+
+    path('update_item/', views.updateItem, name="update_item"),
 ]
--- transaksi/views.py
+++ transaksi/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
 from .models import *
 
 # Create your views here.
 def transaksi(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items =  order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
+
     products    = Product.objects.all()
-    context     = {'products':products}
+    context     = {'products':products, 'cartItems':cartItems}
     return render(request, 'transaksi/transaksi.html', context)
 
 def keranjang(request):
@@ -13,11 +27,13 @@ def keranjang(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items =  order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order,'cartItems':cartItems}
     return render(request, 'transaksi/keranjang.html', context)
 
 def pembayaran(request):
@@ -26,10 +42,37 @@ def pembayaran(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items =  order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'transaksi/pembayaran.html', context)
 
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+    
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
+    return JsonResponse('Item was added', safe=False)
