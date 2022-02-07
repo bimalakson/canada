ccd2ade9c9c9db38916040e2d328409d68b8adac
bimalakson <bimalakson@gmail.com>
2022-02-08 01:14:15 +0700

    third

--- transaksi/views.py
+++ transaksi/views.py
@@ -8,10 +8,28 @@ def transaksi(request):
     return render(request, 'transaksi/transaksi.html', context)
 
 def keranjang(request):
-    context = {}
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items =  order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'transaksi/keranjang.html', context)
 
 def pembayaran(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items =  order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'transaksi/pembayaran.html', context)
 
