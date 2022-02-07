9c679e6ae84730fbde2f9350639030d2a1beb33c
bimalakson <bimalakson@gmail.com>
2022-02-07 16:55:30 +0700

    Second

--- transaksi/views.py
+++ transaksi/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def transaksi(request):
-    context = {}
+    products    = Product.objects.all()
+    context     = {'products':products}
     return render(request, 'transaksi/transaksi.html', context)
 
 def keranjang(request):
